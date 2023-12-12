e6ccf666aab31eec5350c61d09b2641be6ceca0f
AntonBar <BarashASUA@gmail.com>
2023-12-12 21:53:49 +0200

    Initial commit

---
+++ src/main/java/com/example/TGBankingService/TgBankingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.TGBankingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgBankingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgBankingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TGBankingService/TgBankingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TGBankingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgBankingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
