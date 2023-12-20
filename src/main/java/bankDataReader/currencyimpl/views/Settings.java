7cef9699477e069bcc8fdeb0f962e196b48b32a6
khfedor <141046719+khfedor@users.noreply.github.com>
2023-12-20 14:29:08 -0500

    Kris added DTO

--- src/main/java/bankDataReader/currencyimpl/views/Settings.java
+++ src/main/java/bankDataReader/currencyimpl/views/Settings.java
@@ -1,7 +1,30 @@
 package bankDataReader.currencyimpl.views;
 
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Settings {
 
+    static void displaySettingsMethod() {
+        SendMessage sendMessage = new SendMessage();
+        String titleMessage = "Оберіть пункт налаштування";
+        sendMessage.setText(titleMessage);
+        //sendMessage.setChatId(chat.getId());
+
+        InlineKeyboardButton banks = InlineKeyboardButton.builder().text("Банк").callbackData("Банк").build();
+        InlineKeyboardButton currency = InlineKeyboardButton.builder().text("Валюта").callbackData("Валюта").build();
+        InlineKeyboardButton betterOffers = InlineKeyboardButton.builder().text("Кращі пропозиції").callbackData("Кращі пропозиції").build();
+        InlineKeyboardButton notificationsTime = InlineKeyboardButton.builder().text("Час сповіщень").callbackData("Час сповіщень").build();
+
+        InlineKeyboardMarkup inlineKeyboardMarkup = InlineKeyboardMarkup.builder()
+                .keyboard(Collections.singletonList(Arrays.asList(banks, currency, betterOffers, notificationsTime))).build();
+        sendMessage.setReplyMarkup(inlineKeyboardMarkup);
+
+    }
 
 
 }
