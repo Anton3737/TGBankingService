3ddb5adb69765dfb7a0b0bf64596a6738585e8af
YuliaSher <juliafokina1984@gmail.com>
2023-12-22 00:04:32 +0200

    Внесла зміни в GetUrlCurrency class Створила Checkmark class для додавання чи забирання галочки з назви кнопок

--- src/main/java/bankDataReader/currencyimpl/GetUrlCurrency.java
+++ src/main/java/bankDataReader/currencyimpl/GetUrlCurrency.java
@@ -3,57 +3,14 @@ package bankDataReader.currencyimpl;
 import bankDataReader.enums.Currency;
 
 public class GetUrlCurrency {
-    public static String getURL(Currency ccy) {
-        //в String ccy мають приходити дані від натискання кнопки в розділі "вибір валюти"
-        String currency = "";
-        switch (ccy) {
-            case USD:
-                currency = "usd";
-                break;
-            case EUR:
-                currency = "eur";
-                break;
-            case GBP:
-                currency = "gbp";
-                break;
-            case CHF:
-                currency = "chf";
-                break;
-            case SEK:
-                currency = "chf";
-                break;
-            case PLN:
-                currency = "pln";
-                break;
-            case NOK:
-                currency = "nok";
-                break;
-            case JPY:
-                currency = "jpy";
-                break;
-            case DKK:
-                currency = "dkk";
-                break;
-            case CNY:
-                currency = "cny";
-                break;
-            case CAD:
-                currency = "cad";
-                break;
-            case AUD:
-                currency = "aud";
-                break;
-            case HUF:
-                currency = "huf";
-                break;
-            case CZK:
-                currency = "czk";
-                break;
-            case ISL:
-                currency = "ils";
-                break;
-            //default нам не потрібен, бо дані будемо брати з натискання кнопок
-        }
+    /**
+     * getUR вхідні дані - Currency енам, вихідні - String URL
+     * @param ccy
+     * @return
+     */
+    public String getURL(Currency ccy) {
+        String currency = ccy.name().toLowerCase();
+
         return "https://minfin.com.ua/ua/currency/banks/" + currency + "/";
     }
 }
