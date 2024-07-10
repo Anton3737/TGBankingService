4a7ad1808e5c16cb31c157084b3c2fb2151d76f9
Barash Anton <36210606+Anton3737@users.noreply.github.com>
2024-07-10 09:27:38 +0300

    Update LoginConstants.java

--- src/main/java/com/example/TGBankingService/bankDataReader/telegram/LoginConstants.java
+++ src/main/java/com/example/TGBankingService/bankDataReader/telegram/LoginConstants.java
@@ -2,6 +2,6 @@ package com.example.TGBankingService.bankDataReader.telegram;
 
 public class LoginConstants {
 
-    public static final String NAME = "BagHuntersCurrencyBot";
-    public static final String TOKEN = "6887792565:AAG8J5h9sWYpFpirXZ9tW9jIxNojVwW83pg";
+    public static final String NAME = "";
+    public static final String TOKEN = "";
 }
